c321c624a7e92d82add4687a86133b9d16833467
Pietro Donatini <pietro.donatini@unibo.it>
2024-08-14 11:25:18 +0200

    clear_service_name

--- app/models/software.rb
+++ app/models/software.rb
@@ -8,4 +8,19 @@ class Software < ApplicationRecord
 
   def self.get_tech_from_open_port(service_name, port)
   end
+
+  def self.clear_service_name(str)
+    case str
+    when /^node /
+      "node"
+    when /python/
+      "python"
+    when /in\/apach/
+      "apache"
+    when "apache2"
+      "apache"
+    else
+      str
+    end
+  end
 end
