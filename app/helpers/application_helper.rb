5c53eca6a45199cc6b38a784b7861bb278c7990e
Pietro Donatini <pietro.donatini@unibo.it>
2024-09-04 14:58:35 +0200

    web_site_state_color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  def web_site_state_color(web_site)
+    case web_site.state
+    when "down"
+      "danger"
+    when "unhealthy"
+      "warning"
+    else
+      ""
+    end
+  end
 end
 
 include DmUniboCommonHelper
