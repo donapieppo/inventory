713b23d5973f238ad9cdd06c0ffec18c5bd3c239
Pietro Donatini <pietro.donatini@unibo.it>
2024-09-04 14:58:50 +0200

    requests

--- app/models/web_site.rb
+++ app/models/web_site.rb
@@ -1,3 +1,5 @@
+# http://frontend-http-01.unibo.it:8080/api/9/http/upstreams
+# https://nginx.org/en/docs/http/ngx_http_api_module.html#def_nginx_http_upstream
 class WebSite < ApplicationRecord
   has_many :web_site_addresses
 
@@ -13,5 +15,9 @@ class WebSite < ApplicationRecord
     ip = IPAddr.new(ip).to_i
     w = web_site_addresses.find_or_create_by!(ip: ip, port: port.to_i)
     w.update(updated_at: Time.now) unless w.new_record?
+  end
+
+  def requests
+    arch_requests + new_requests
   end
 end
