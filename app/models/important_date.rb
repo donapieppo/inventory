b58ef0042154d4c82cb7841a8a9cc6daaa4fcd38
Pietro Donatini <pietro.donatini@unibo.it>
2024-09-13 12:16:01 +0200

    important dates

--- app/models/important_date.rb
+++ app/models/important_date.rb
@@ -1,6 +1,8 @@
 class ImportantDate < ApplicationRecord
   belongs_to :datable, polymorphic: true
 
+  validates :date_type, presence: true
+
   def to_s
     "#{I18n.l date} #{name} #{date_type}"
   end
